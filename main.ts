8ead701c9e529e7b1c5100b49976681a8528f8e9
JeremyTsui <44908010+rdhuht@users.noreply.github.com>
2020-12-28 21:37:02 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,4 @@
-timeanddate.onMinuteChanged(function () {
-    music.startMelody(music.builtInMelody(Melodies.JumpUp), MelodyOptions.Once)
-})
-timeanddate.set24HourTime(12, 12, 12)
+timeanddate.set24HourTime(13, 30, 0)
 let tm = TM1637.create(
 DigitalPin.P1,
 DigitalPin.P2,
