433f530d6fbcff777a4a7b157f8d91981031aa95
JeremyTsui <44908010+rdhuht@users.noreply.github.com>
2020-12-28 22:08:18 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,3 @@
-timeanddate.set24HourTime(13, 30, 0)
 let tm = TM1637.create(
 DigitalPin.P1,
 DigitalPin.P2,
@@ -7,5 +6,5 @@ DigitalPin.P2,
 )
 tm.showDP(0, true)
 basic.forever(function () {
-    tm.showNumber(parseFloat("" + convertToText(parseFloat(timeanddate.time(timeanddate.TimeFormat.HMM).substr(0, 2))) + convertToText(parseFloat(timeanddate.time(timeanddate.TimeFormat.HMM).substr(3, 2)))))
+    tm.showNumber(0)
 })
